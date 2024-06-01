2903ffd16be0b6a59940f244ca9a1875bd5edb51
duranangie <duranangie>
2024-06-01 01:16:20 -0500

    feat: :sparkles: clase main y clase auto con db

---
+++ src/main/java/logica/Auto.java
@@ -0,0 +1,92 @@
+    package logica;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+public class Auto implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String modelo;
+    private String marca;
+    private String motor;
+    private String color;
+    private int patente;
+    private String cantidadPuertas;
+
+    public Auto() {
+    }
+
+    public Auto(int id, String modelo, String marca, String motor, String color, int patente, String cantidadPuertas) {
+        this.id = id;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.motor = motor;
+        this.color = color;
+        this.patente = patente;
+        this.cantidadPuertas = cantidadPuertas;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPatente() {
+        return patente;
+    }
+
+    public void setPatente(int patente) {
+        this.patente = patente;
+    }
+
+    public String getCantidadPuertas() {
+        return cantidadPuertas;
+    }
+
+    public void setCantidadPuertas(String cantidadPuertas) {
+        this.cantidadPuertas = cantidadPuertas;
+    }
+    
+    
+    
+}
+
+
---
+++ src/main/java/main/concesionaria/Concesionaria.java
@@ -0,0 +1,15 @@
+package main.concesionaria;
+
+import igu.Principal;
+
+public class Concesionaria {
+
+    public static void main(String[] args) {
+        
+        Principal principal = new Principal();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+                
+
+    }
+}
