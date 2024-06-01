5e862bb26af2d689b2fbb805f087bda8288469b6
duranangie <duranangie>
2024-06-01 01:19:40 -0500

    feat: :sparkles: clase de auto

---
+++ target/generated-sources/annotations/logica/Auto_.java
@@ -0,0 +1,19 @@
+package logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-01T01:12:32", comments="EclipseLink-2.7.10.v20211216-rNA")
+@StaticMetamodel(Auto.class)
+public class Auto_ { 
+
+    public static volatile SingularAttribute<Auto, String> marca;
+    public static volatile SingularAttribute<Auto, String> motor;
+    public static volatile SingularAttribute<Auto, String> color;
+    public static volatile SingularAttribute<Auto, Integer> id;
+    public static volatile SingularAttribute<Auto, String> cantidadPuertas;
+    public static volatile SingularAttribute<Auto, String> modelo;
+    public static volatile SingularAttribute<Auto, Integer> patente;
+
+}
